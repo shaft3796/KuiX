3fb9c57068aec75f5a6de6d33c828e51112d3a15
Shaft <sh4ft.me@gmail.com>
2023-05-24 17:55:23 +0200

    ALPHA-1.0 Dev-0.4 [+] Completed implementation of the Logger

--- kuix/core/logger.py
+++ kuix/core/logger.py
@@ -1,7 +1,10 @@
 """
 Implements a custom loging system
 """
+import json
+import os
 import sys
+import time
 from datetime import datetime
 
 from colorama import Fore, Style, Back
@@ -41,7 +44,7 @@ class Levels:
     INFO = "INFO", Fore.GREEN
     WARNING = "WARNING", Fore.YELLOW
     ERROR = "ERROR", Fore.RED
-    CRITICAL = "CRITICAL", Fore.WHITE + Back.RED
+    CRITICAL = "CRITICAL", Fore.BLACK + Back.RED
     DEBUG = "DEBUG", Fore.CYAN
 
 
@@ -73,5 +76,106 @@ class Log:
 # -- Logger --
 class Logger(Lockable):
 
-    def __init__(self):
+    def __init__(self, path=None):
+        """
+        Initializes the logger.
+        :param path: None by default, if modified, the logger will use this path as the root path to save logs.
+
+        """
         super().__init__()
+
+        # --- I/O ---
+        self.io = KuixIO()
+
+        # --- Logs ---
+        self.is_verbose = False
+        self.is_debug = False
+
+        # -- Persistence --
+        self.root_path = path if path is None else (path if path.endswith("/") else path + "/")
+        self.files = {}
+        if self.root_path is not None:
+            os.makedirs(self.root_path, exist_ok=True)
+            for level in Levels.__dict__.keys():
+                if not level.startswith("_"):
+                    self.files[level] = open(self.root_path + level.lower() + ".json", "a")
+
+    def enable_verbose(self):
+        """
+        Enables verbose mode.
+        """
+        self.is_verbose = True
+
+    def enable_debug(self):
+        """
+        Enables debug mode.
+        """
+        self.is_debug = True
+
+    @Lockable.locked
+    def log(self, data: (str, Exception), level=Levels.INFO):
+        """
+        Logs data to the console and to the log files.
+        :param data: Data to be logged, a string or an exception.
+        :param level: Level of the log, import src.core.logger.Levels to use the predefined levels.
+        :raises KuixException: If the level is not valid.
+        """
+        if level not in Levels.__dict__.values():
+            raise KuixException(f"Invalid log level: {level}, use src.core.logger.Levels to use the predefined levels.")
+
+        if (level == Levels.DEBUG and not self.is_debug) or (level == Levels.TRACE and not self.is_verbose):
+            return
+
+        try:
+            log = Log(data, level, int(time.time()))
+            # - Terminal -
+            print(log.to_string())
+
+            # - Files -
+            if self.root_path is not None:
+                try:
+                    json.dump(log.to_dict(), self.files[level], indent=4)
+                except Exception as e:
+                    print(f"Error while writing to log file: {e}")
+        except Exception as e:
+            print(f"Error while logging: {e}")
+
+    # --- Shortcuts ---
+    def trace(self, data: (str, Exception)):
+        self.log(data, Levels.TRACE)
+
+    def info(self, data: (str, Exception)):
+        self.log(data, Levels.INFO)
+
+    def warning(self, data: (str, Exception)):
+        self.log(data, Levels.WARNING)
+
+    def error(self, data: (str, Exception)):
+        self.log(data, Levels.ERROR)
+
+    def critical(self, data: (str, Exception)):
+        self.log(data, Levels.CRITICAL)
+
+    def debug(self, data: (str, Exception)):
+        self.log(data, Levels.DEBUG)
+
+    # --- Destructor ---
+    def __del__(self):
+        self.io.close()
+        for file in self.files.values():
+            file.close()
+
+
+def _exception_hook(exc_type, value, traceback):
+    logger.critical(value)
+
+
+def kuix_override():
+    sys.stdout = kuix_io
+    sys.excepthook = _exception_hook
+
+
+# -- Global --
+kuix_io = KuixIO()
+logger = Logger()
+kuix_override()
